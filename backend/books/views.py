d7b25336b030738991599ebc0a48af34d976188d
yaofeng-wang <yaofeng96@gmail.com>
2021-01-07 10:10:25 +0800

    update repo

--- backend/books/views.py
+++ backend/books/views.py
@@ -6,5 +6,10 @@ from .models import Book
 def home(request):
     books = Book.objects.all()
     context = {'books': books}
-
     return render(request, 'home.html', context=context)
+
+
+def displayBook(request, id):
+    book = Book.objects.get(pk=id)
+    context = {'book': book}
+    return render(request, 'display_book.html', context=context)
