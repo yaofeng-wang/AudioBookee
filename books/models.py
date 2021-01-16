05b53617942c8f11295002aac2cb533e0c4fc09e
yaofeng-wang <yaofeng96@gmail.com>
2021-01-16 17:11:59 +0800

    update books model

--- books/models.py
+++ books/models.py
@@ -4,28 +4,23 @@ from backend.settings import MEDIA_ROOT
 from users.models import UserProfile
 from django.urls import reverse
 from django.conf import settings
+from django_resized import ResizedImageField
 
 
 class Book(models.Model):
-
-    IMAGE_WIDTH = 400
-    IMAGE_HEIGHT = 500
 
     seller = models.ForeignKey(to=UserProfile,
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
-    image = models.ImageField(upload_to='books')
+    thumbnail = ResizedImageField(
+        size=[300, 400], upload_to='books', quality=75, force_format='JPEG')
+    image = ResizedImageField(
+        size=[500, 300], upload_to='books', quality=75, force_format='JPEG')
 
     def __str__(self):
         return f'{self.title}'
 
     def get_absolute_url(self):
         return reverse('display_book', args=[self.id])
-
-    def save(self, *args, **kwargs):
-        # save instance to create image in static folder
-        super().save(*args, **kwargs)
-
-        # image re-sizing temporarily removed
