8112368e2f0688efe279c42bb27be453f15ffeca
yaofeng-wang <yaofeng96@gmail.com>
2021-01-16 17:11:29 +0800

    update books admin page

--- books/admin.py
+++ books/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Book
+from django.utils.html import format_html
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['admin_thumbnail', 'title', 'price', 'seller']
+
+    def admin_thumbnail(self, obj):
+        return format_html('<img src="{}" style="height:150px; width:150px;" />'.format(obj.thumbnail.url))
+    admin_thumbnail.short_description = 'Admin page thumbnail'
+
+
+admin.site.register(Book, BookAdmin)
